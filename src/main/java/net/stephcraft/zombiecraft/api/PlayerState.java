e159d271fb4d50a30768d52e0c40cffdbd6213aa
Stephcraft <contact@stephcraft.net>
2021-11-10 19:32:02 -0500

    Includes bukkit

--- src/main/java/net/stephcraft/zombiecraft/api/PlayerState.java
+++ src/main/java/net/stephcraft/zombiecraft/api/PlayerState.java
@@ -7,9 +7,12 @@ public class PlayerState {
 		
 		org.bukkit.Location l;
 		
+		org.bukkit.entity.Player p;
+		org.bukkit.block.Block b;
+		
 		org.bukkit.command.BlockCommandSender s;
 		
-		me.lucko.commodore.BrigadierUnsupportedException b;
+		me.lucko.commodore.BrigadierUnsupportedException nb;
 		// net.stephcraft.zombiecraft.api.commodore.BrigadierUnsupportedException c;
 	}
 }
--- src/main/java/net/stephcraft/zombiecraft/api/ZombiecraftEasterEgg.java
+++ src/main/java/net/stephcraft/zombiecraft/api/ZombiecraftEasterEgg.java
@@ -1,5 +1,8 @@
 package net.stephcraft.zombiecraft.api;
 
+import net.stephcraft.zombiecraft.api.minecraft.Block;
+
 public abstract class ZombiecraftEasterEgg {
 	
+	public abstract Block getBlock(int x, int y, int z);
 }
